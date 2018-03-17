47349cd08b04dcbc99db09020c1406184571a670
mateyo3 <mateoreyes720@gmail.com>
2018-03-17 08:06:19 -0700

    StaticComponent-4 with ArticleRow component breakdown

--- natgeo/src/Static/StaticComponent-4.jsx
+++ natgeo/src/Static/StaticComponent-4.jsx
@@ -5,7 +5,7 @@ const StaticComponent = (props) => {
 		<div>
 			<NavBar />
 			<Banner />
-			<Articles />
+			<ArticleRow />
 			<Footer />
 	    </div>
 	);
@@ -93,60 +93,42 @@ const BannerImage = (props) => {
 }
 
 // HOLDS ALL ARTLCE COMPONENTS
-const Articles = () => {
+const ArticleRow = () => {
 	return (
-	  <div className="row">
-			<ArticleLeft />
-			<ArticleCenter />
-			<ArticleRight />
-	  </div>
-	);
-}
-
-
-const ArticleLeft = () => {
-	return (
-		<div className="col-md-3">
-			<Article
-				className={"side-image"}
-				src={"https://yourshot.nationalgeographic.com/u/fQYSUbVfts-T7odkrFJckdiFeHvab0GWOfzhj7tYdC0uglagsDNfNuDs6wgwFZpcVHujEFyJtcjiC5Jm1ob5No2-BIY3xpwgi4VxF-VvhgFraXN6TN8WL5WytlCgwmYZkLhu_jFShzbLcwrgM5rW5Iy6kZBrEEg0ErybNGrEfo6YvfZWwPIrerUhZ-keroeLRpNoPYxSVnWCsdM2N4VbbgipP1cNl6l1/"}
-				title={"FIERCE LOOK"}
-				description={"A black hawk-eagle takes a shower in Misiones, Argentina. Her eyes are momentarily protected by a nictitating membrane."}
-				buttonName={"Read More"}
-			/>
+		<div className="row">
+			<div className="col-md-3">
+				<Article 
+					className={"side-image"}
+					src={"https://yourshot.nationalgeographic.com/u/fQYSUbVfts-T7odkrFJckdiFeHvab0GWOfzhj7tYdC0uglagsDNfNuDs6wgwFZpcVHujEFyJtcjiC5Jm1ob5No2-BIY3xpwgi4VxF-VvhgFraXN6TN8WL5WytlCgwmYZkLhu_jFShzbLcwrgM5rW5Iy6kZBrEEg0ErybNGrEfo6YvfZWwPIrerUhZ-keroeLRpNoPYxSVnWCsdM2N4VbbgipP1cNl6l1/"}
+					title={"FIERCE LOOK"}
+					description={"A black hawk-eagle takes a shower in Misiones, Argentina. Her eyes are momentarily protected by a nictitating membrane."}
+					buttonName={"Read More"}
+				/>
+			</div>
+			<div className="col-md-6">
+				<Article 
+					className={"center-image"}
+					src={"https://yourshot.nationalgeographic.com/u/fQYSUbVfts-T7odkrFJckdiFeHvab0GWOfzhj7tYdC0uglagsDNfN00ztqP4IFKCr4JcjiJapY7BlMmanMMt8V8wsKyUci7yVzi5SG3TS7kijrqu0gYfBiROh-79GNIdU3Z5Tabkv4YPS9duBmFDrZcGKigk5K8R7sdMTxK9OTchEABlJV75BsngYUqIbI5Yx3mIWjeDdHVHB1WyII9UBo_sPBjJboYS/"}
+					title={"STORM BELOW"}
+					description={"A summer storm settles in over Beijing, China. Your Shot photographer Qing Hu captured this dramatic scene by using a long exposure of three seconds."}
+					buttonName={"Read More"}
+					/>
+			</div>
+			<div className="col-md-3">
+				<Article 
+					className={"side-image"}
+					src={"https://yourshot.nationalgeographic.com/u/fQYSUbVfts-T7odkrFJckdiFeHvab0GWOfzhj7tYdC0uglagsDNfMsvLk0GtR_xRU0VYJmz8f7IIMZ02xhzCdgvR15Y-pBlXnwQIlHKoeqSjsHgodjvpoRUlCvmkI1LmGwg_fE9Bo3rA0ZW3C8f1NJRmNGJRZKrKKNq5YLrmDkRWOu663PqjnKEujlERztg3Mzm4Ei0X22eVs2UXL-3GjX9ET8u935Eb/"}
+					title={"ICE TUBE"}
+					description={"Mer de Glace is a glacier in the French Alps. Here, adventurers climb up a moulin, a vertical shaft inside of the glacier, to emerge back on top."}
+					buttonName={"Read More"}
+				/>
+			</div>
 		</div>
 	);
 }
 
-const ArticleCenter = () => {
-	return (
-		<div className="col-md-6">
-			<Article
-				className={"center-image"}
-				src={"https://yourshot.nationalgeographic.com/u/fQYSUbVfts-T7odkrFJckdiFeHvab0GWOfzhj7tYdC0uglagsDNfN00ztqP4IFKCr4JcjiJapY7BlMmanMMt8V8wsKyUci7yVzi5SG3TS7kijrqu0gYfBiROh-79GNIdU3Z5Tabkv4YPS9duBmFDrZcGKigk5K8R7sdMTxK9OTchEABlJV75BsngYUqIbI5Yx3mIWjeDdHVHB1WyII9UBo_sPBjJboYS/"}
-				title={"STORM BELOW"}
-				description={"A summer storm settles in over Beijing, China. Your Shot photographer Qing Hu captured this dramatic scene by using a long exposure of three seconds."}
-				buttonName={"Read More"}
-			/>
-		</div>
-	);
-}
 
-const ArticleRight = () => {
-	return (
-		<div className="col-md-3">
-			<Article
-				className={"side-image"}
-				src={"https://yourshot.nationalgeographic.com/u/fQYSUbVfts-T7odkrFJckdiFeHvab0GWOfzhj7tYdC0uglagsDNfMsvLk0GtR_xRU0VYJmz8f7IIMZ02xhzCdgvR15Y-pBlXnwQIlHKoeqSjsHgodjvpoRUlCvmkI1LmGwg_fE9Bo3rA0ZW3C8f1NJRmNGJRZKrKKNq5YLrmDkRWOu663PqjnKEujlERztg3Mzm4Ei0X22eVs2UXL-3GjX9ET8u935Eb/"}
-				title={"ICE TUBE"}
-				description={"Mer de Glace is a glacier in the French Alps. Here, adventurers climb up a moulin, a vertical shaft inside of the glacier, to emerge back on top."}
-				buttonName={"Read More"}
-			/>
-		</div>
-	);
-}
-
-// PULLS PROPERTIES FROM PARENT COMPONENT, ArticleRight, ArticleCenter, ArticleLeft
+// PULLS PROPERTIES FROM PARENT COMPONENT, ArticlesRow
 const Article = (props) => {
 	return (
         <div>
